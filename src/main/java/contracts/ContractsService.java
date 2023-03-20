7a8c9bf6f43217f0427d7157b8228feea7b399d9
Emmanuele Ottanà <manu91eo@gmail.com>
2023-03-20 14:04:36 +0100

    sistemata classe enum

--- src/main/java/contracts/ContractsService.java
+++ src/main/java/contracts/ContractsService.java
@@ -4,35 +4,5 @@ import static contracts.TypeOfContract.*;
 
 public class ContractsService {
     ContractsEntity contractsEntity;
-    /**
-     * Added Class method getTypeOfContract for switch
-     * @param contract of the single employee
-     */
-    public void getTypeOfContract(ContractsEntity contract) {
-        contractsEntity.setTypeOfContract(contract);
-        if (contract.equals(FIXED_TERM)) {
-            System.out.println("Contratto a tempo determinato");
-        } else if (contract.equals(OPEN_ENDED)) {
-            System.out.println("Contratto a tempo indeterminato");
-        } else if (contract.equals(PART_TIME)) {
-            System.out.println("Contratto part Time");
-        }else if (contract.equals(FULL_TIME)) {
-            System.out.println("Contratto full Time");
-        } else if (contract.equals(APPRENTICESHIP)) {
-            System.out.println("Contratto di apprendistato");
-        } else if (contract.equals(INTERNSHIP)) {
-            System.out.println("Contratto di stage");
-        } else if (contract.equals(ON_CALL_CONTRACT)) {
-            System.out.println("Contratto Intermittente");
-        } else if (contract.equals(OCCASIONAL_PROFESSIONAL_SERVICE)) {
-            System.out.println("Contratto a Prestazioni Occasionali");
-        } else if (contract.equals(AUXILIARY)) {
-            System.out.println("Contratto di tipo Ausiliario");
-        } else if (contract.equals(TERM_CONTRACT)) {
-            System.out.println("Contratto Co Co Co");
-        } else if (contract.equals(ADMINISTRATION)) {
-            System.out.println("Contratto a Somministrazione");
-        }
-    }
 
 }
--- src/main/java/contracts/TypeOfContract.java
+++ src/main/java/contracts/TypeOfContract.java
@@ -1,15 +1,24 @@
 package contracts;
 
 public enum TypeOfContract {
-    FIXED_TERM,
-    OPEN_ENDED,
-    PART_TIME,
-    FULL_TIME,
-    APPRENTICESHIP,
-    INTERNSHIP,
-    ON_CALL_CONTRACT,
-    OCCASIONAL_PROFESSIONAL_SERVICE,
-    SELF_EMPLOYMENT,
-    TERM_CONTRACT,
-    AGENCY_WORK;
+    AGENCY_WORK("Contratto a somministrazione"),
+    APPRENTICESHIP("Contratto di apprendistato"),
+    FIXED_TERM("Contratto a tempo determinato"),
+    FULL_TIME("Contratto full Time"),
+    INTERNSHIP("Contratto di stage"),
+    OCCASIONAL_PROFESSIONAL_SERVICE("Contratto a Prestazioni Occasionali"),
+    ON_CALL_CONTRACT("Contratto Intermittente"),
+    OPEN_ENDED("Contratto a tempo indeterminato"),
+    PART_TIME("Contratto part Time"),
+    SELF_EMPLOYMENT("Contratto di lavoro autonomo"),
+    TERM_CONTRACT("Contratto Co Co Co");
+
+    private String displayType;
+
+    public String getDisplayType() { return displayType; }
+
+    TypeOfContract(String typeOfContract) {
+        this.displayType = typeOfContract;
+    }
+
 }
